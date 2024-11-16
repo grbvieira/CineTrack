60cd938c71a646881aa24336738c02a0b047b8f1
Gerson Vieira <gersonbase@gmail.com>
2024-11-16 18:45:50 -0400

    Remove files that should be ignored

--- CineTrack/Pods/Target Support Files/Pods-CineTrack-CineTrackUITests/Pods-CineTrack-CineTrackUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CineTrack_CineTrackUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CineTrack_CineTrackUITests
-@end
--- CineTrack/Pods/Target Support Files/Pods-CineTrackTests/Pods-CineTrackTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CineTrackTests : NSObject
-@end
-@implementation PodsDummy_Pods_CineTrackTests
-@end
