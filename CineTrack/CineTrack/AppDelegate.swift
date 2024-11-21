72d5837a93f4ddbdfecfa6bf2f8da1d0a689952b
Gerson Vieira <gersonbase@gmail.com>
2024-11-21 02:00:46 -0400

    Add Appdelegate firebase

--- CineTrack/CineTrack/AppDelegate.swift
+++ CineTrack/CineTrack/AppDelegate.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+//import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+      //  FirebaseApp.configure()
+        
+        return true
+    }
+    // MARK: URL Schemes
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+//        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+//            return true
+//        }
+        // other URL handling goes here.
+        return false
+    }
+}
