cf90f510c58f75dfd038de1fc66d948bb2b5a809
Gerson Vieira <gersonbase@gmail.com>
2024-11-21 02:32:30 -0400

    option to navigate to both screens

--- CineTrack/CineTrack/CineTrackApp.swift
+++ CineTrack/CineTrack/CineTrackApp.swift
@@ -11,7 +11,27 @@ import SwiftUI
 struct CineTrackApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StartView()
         }
     }
+    
+   /* func signUp(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let user = authResult?.user {
+                completion(.success(user))
+            }
+        }
+    }
+
+    func signIn(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let user = authResult?.user {
+                completion(.success(user))
+            }
+        }
+    }*/
 }
--- CineTrack/CineTrack/StartView.swift
+++ CineTrack/CineTrack/StartView.swift
@@ -8,33 +8,18 @@
 import SwiftUI
 
 struct StartView: View {
+    
     var body: some View {
         NavigationView {
-            VStack(spacing: 30) {
-                Text("Choose an Option")
-                    .font(.system(size: 24, weight: .bold, design: .rounded))
-                    .foregroundColor(.black)
-                    .padding(.top, 40)
-
-                // Navegação para LoginView
-                NavigationLink(destination: LoginView()) {
-                    CustomButton(title: "Go to LoginView",
-                                 width: 200,
-                                 height: 50,
-                                 gradientColors: [.blue, .green]) {}
-                }
-
-                // Navegação para LoginViewAnimation
+            List {
                 NavigationLink(destination: LoginAnimationView()) {
-                    CustomButton(title: "Go to LoginViewAnimation",
-                                 width: 200,
-                                 height: 50,
-                                 gradientColors: [.purple, .pink]) {}
+                    Text("Animar Login")
+                }
+                NavigationLink(destination: LoginView()) {
+                    Text("Login Simples")
                 }
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color(.systemGray6))
-            .ignoresSafeArea()
+            .navigationBarTitle("Escolha a Opção")
         }
     }
 }
