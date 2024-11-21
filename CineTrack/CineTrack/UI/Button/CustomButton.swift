7691e4b55fc8362d42b6c83cde14325e182d7257
Gerson Vieira <gersonbase@gmail.com>
2024-11-21 02:00:12 -0400

    CustomButton

--- CineTrack/CineTrack/UI/Button/CustomButton.swift
+++ CineTrack/CineTrack/UI/Button/CustomButton.swift
@@ -5,4 +5,39 @@
 //  Created by Gerson Vieira on 21/11/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomButton: View {
+    var title: String
+    var width: CGFloat? = nil
+    var height: CGFloat? = nil
+    var gradientColors: [Color]? = nil // Permite botão sem gradiente
+    var cornerRadius: CGFloat = 10
+    var textColor: Color = .white
+    var action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .bold()
+                .frame(
+                    width: width,
+                    height: height
+                )
+                .background(
+                    gradientColors != nil
+                        ? RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                            .fill(
+                                LinearGradient(
+                                    colors: gradientColors!,
+                                    startPoint: .top,
+                                    endPoint: .bottomTrailing
+                                )
+                            )
+                        : nil // Sem fundo se não houver gradiente
+                )
+                .foregroundColor(textColor)
+        }
+    }
+}
+
