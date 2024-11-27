dbecda0aa389a4158ce463f050e634856eef3aa7
Gerson Vieira <gersonbase@gmail.com>
2024-11-27 02:18:27 -0400

    Add ProfileView

--- CineTrack/CineTrack/Core/Authentication/View/ProfileView.swift
+++ CineTrack/CineTrack/Core/Authentication/View/ProfileView.swift
@@ -5,4 +5,91 @@
 //  Created by Gerson Vieira on 27/11/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ProfileView: View {
+    
+    var body: some View {
+        List {
+            Section {
+                HStack {
+                    Text("J")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 72,height: 72)
+                        .background(Color(.systemGray))
+                        .clipShape(.circle)
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Michael Jordan")
+                            .fontWeight(.semibold)
+                            .padding(.top, 4)
+                        
+                        Text("michaeljordam@gmail.com")
+                            .font(.subheadline)
+                            .accentColor(.gray)
+                    }
+                }
+            }
+            
+            Section("General") {
+                HStack(spacing: 12) {
+                    Image(systemName: "gear")
+                        .imageScale(.small)
+                        .font(.title)
+                        .foregroundStyle(Color(.systemGray))
+                    
+                    Text("Version")
+                        .font(.subheadline)
+                        .accentColor(.black)
+                    
+                    Spacer()
+                    
+                    Text("1.0.0")
+                        .font(.subheadline)
+                        .accentColor(.gray)
+                }
+            }
+            
+            Section("Account") {
+                Button {
+                    // Signt out ..
+                } label: {
+                    HStack(spacing: 12) {
+                        Image(systemName: "arrow.left.circle.fill")
+                            .imageScale(.small)
+                            .font(.title)
+                            .foregroundStyle(Color(.systemRed))
+                        
+                        Text("Sign Out")
+                            .font(.subheadline)
+                            .accentColor(.black)
+                    }
+                }
+                
+                Button {
+                    // Delete Account ..
+                } label: {
+                    HStack(spacing: 12) {
+                        Image(systemName: "arrow.left.circle.fill")
+                            .imageScale(.small)
+                            .font(.title)
+                            .foregroundStyle(Color(.systemRed))
+                        
+                        Text("Delete Account")
+                            .font(.subheadline)
+                            .accentColor(.black)
+                    }
+                }
+            }
+        }
+        
+        
+    }
+}
+struct ProfileView_Preview: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
