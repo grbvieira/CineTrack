9a936542cef1b230423bcfce4266be1633daac8b
Gerson Vieira <gersonbase@gmail.com>
2024-11-27 02:18:13 -0400

    Add ViewModel for Authentication

--- CineTrack/CineTrack/Core/Authentication/ViewModel/AuthViewModel.swift
+++ CineTrack/CineTrack/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -5,20 +5,53 @@
 //  Created by Gerson Vieira on 21/11/24.
 //
 
-import FirebaseCore
-import FirebaseEmailAuthUI
+import FirebaseAuth
+import FirebaseFirestore
 
-protocol LoginViewModelContract {
-    func loginEmail()
+@MainActor
+protocol AuthViewModelContract {
+    func signIn(withEmail email: String, password: String) async throws
+    func createUser(withEmail email: String, password: String, fullName: String) async throws
+    func signOut()
+    func deleAccount()
+    func fetchUser() async
 }
 
-class LoginViewModel {
+class AuthViewModel: ObservableObject, AuthViewModelContract{
+    @Published var userSession: FirebaseAuth.User?
+    @Published var currentUser: User?
+     
+    init() {
+        self.userSession = Auth.auth().currentUser
+    }
     
-//    let provider: FUIEmailAuth = {
-//        return FUIEmailAuth(authAuthUI: <#T##FUIAuth#>,
-//                            signInMethod: <#T##String#>,
-//                            forceSameDevice: <#T##Bool#>,
-//                            allowNewEmailAccounts: <#T##Bool#>,
-//                            actionCodeSetting: <#T##FIRActionCodeSettings#>)
-//    }()
+    func signIn(withEmail email: String, password: String) async throws {
+        
+    }
+    
+    func createUser(withEmail email: String, password: String, fullName: String) async throws {
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession =  result.user
+            let user = User(id: result.user.uid, fullName: fullName, email: email)
+            let encoderUser = try Firestore.Encoder().encode(user)
+            try await Firestore.firestore().collection("users").document(user.id).setData(encoderUser)
+        } catch {
+            print ("DEBUG: Failed to create user with error: \(error.localizedDescription)")
+        }
+    }
+    
+    func signOut() {
+        
+    }
+    
+    func deleAccount() {
+        
+    }
+    
+    func fetchUser() async {
+        
+    }
+    
+    
 }
