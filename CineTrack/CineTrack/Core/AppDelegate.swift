879272babe4f62f0f3240a8a05f5aea9f3e9c925
Gerson Vieira <gersonbase@gmail.com>
2024-12-08 01:21:14 -0400

    Add Appdelegate

--- CineTrack/CineTrack/Core/AppDelegate.swift
+++ CineTrack/CineTrack/Core/AppDelegate.swift
@@ -5,4 +5,22 @@
 //  Created by Gerson Vieira on 02/12/24.
 //
 
-import Foundation
+import UIKit
+import FirebaseCore
+import GoogleSignIn
+
+//@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("App Did Lauch!")
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
+    }
+}
