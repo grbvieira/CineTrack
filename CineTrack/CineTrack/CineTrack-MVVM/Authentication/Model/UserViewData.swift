075186c3d4f3e432572d0aae9d0be84a8de917d3
Gerson Vieira <gersonbase@gmail.com>
2024-12-08 01:22:50 -0400

    remove unused cocoapods and codes

--- CineTrack/CineTrack/CineTrack-MVVM/Authentication/Model/UserViewData.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UserViewData.swift
-//  CineTrack
-//
-//  Created by Gerson Vieira on 06/12/24.
-//
-
-import Foundation
--- CineTrack/CineTrack/Core/Authentication/ViewModel/AuthViewModel+GoogleSignIn.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AuthViewModel+GoogleSignIn.swift
-//  CineTrack
-//
-//  Created by Gerson Vieira on 02/12/24.
-//
-
-import Foundation
